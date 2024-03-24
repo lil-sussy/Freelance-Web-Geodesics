3907f4125f9ff657b63745d8adc00e85ac2328e8
lil-sussy <naykeysnet@gmail.com>
2024-03-25 00:04:53 +0100

    navbar done

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,6 +1,11 @@
-
+<script lang="ts">
+  import logo from '$lib/images/webgeodesicslogo.svg';
+</script>
 
 <div class="navbar">
+  <div class="item logo-items">
+    <img class="logo" src={logo} alt=""/>
+  </div>
   <div class="item selected">YAN REGOJO</div>
   <div class="item">PLAYBOOK</div>
   <div class="item">SOCIALS</div>
@@ -11,13 +16,23 @@
 <style lang="scss">
   .navbar {
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-between;
     width: 100%;
     align-items: flex-start;
     gap: 385px 4px;
     position: absolute;
     top: 2rem;
     left: 0px;
+    .logo-items {
+      align-self: flex-start;
+    }
+    .logo {
+      position: relative;
+      top: -1.2rem;
+      left: 2rem;
+      width: 4rem;
+      height: 4rem;
+    }
     .selected::before {
       content: "";
       position: absolute;
@@ -31,17 +46,19 @@
   }
   .item {
     position: relative;
-    width: fit-content;
+    width: 10rem;
     font-family: "Segoe UI", Helvetica;
     font-weight: 300;
     color: #ffffff;
     font-size: 1rem;
     letter-spacing: 0;
     line-height: normal;
+    transition: text-decoration 0.2s;
   }
 
   .item:hover {
     color: #deec48;
+    text-decoration: underline;
     cursor: pointer;
   }
 
