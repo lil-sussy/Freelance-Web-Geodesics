583f243b313077e955fa768ec804190bd331d400
lil-sussy <naykeysnet@gmail.com>
2024-03-25 01:00:40 +0100

    background done

--- src/Background.svelte
+++ src/Background.svelte
@@ -1,59 +1,24 @@
 <script lang="ts">
-  import { interpolatePath } from 'd3-interpolate-path';
-  import { tweened } from 'svelte/motion';
-  import { cubicInOut } from 'svelte/easing';
-  import { onMount } from 'svelte';
+  import PotatoShape from './PotatoShape.svelte';
+import type { PotatoShapeList } from './PotatoShape.svelte';
 
-  // Example paths to morph between
-  const startPath = 'M10,80 Q100,10 190,80 T370,80';
-  const endPath = 'M20,90 Q110,20 200,90 T380,90';
-
-  // Create the interpolator function
-  const pathInterpolator = interpolatePath(startPath, endPath);
-
-  function generateRandomPotatoShape() {
-    const points = 5; // Number of curves to create the shape
-    let path = `M${Math.random() * 100},${Math.random() * 100}`; // Start point
-
-    for (let i = 0; i < points; i++) {
-        const controlX = Math.random() * 100;
-        const controlY = Math.random() * 100;
-        const endX = Math.random() * 100;
-        const endY = Math.random() * 100;
-        path += ` Q${controlX},${controlY} ${endX},${endY}`;
+  const potato1: PotatoShapeList = {
+    1: {
+      startPath: "M89.5703 253.252C-27.7027 189.813 -4.78294 120.906 20.0469 58.9258C49.4606 19.1856 134.264 -7.06486 222.123 1.68529C309.982 10.4354 428.783 16.2689 583.11 17.7272C737.437 19.1856 774.109 13.3522 866.934 26.4774C959.759 39.6026 1030.43 73.874 1084.29 120.906C1138.15 167.938 1133.57 290.076 1099.57 487.318C1065.57 684.561 944.097 618.571 812.69 590.497C681.283 562.424 717.191 542.007 602.974 431.536C488.757 321.066 206.843 316.691 89.5703 253.252Z",
+      endPath: "M58.4989 379C-58.7741 315.561 33.6703 124.98 58.5001 63C87.9139 23.2598 225.639 42.7499 313.499 51.5C401.358 60.2502 517.672 -0.458362 671.999 0.999996C826.326 2.45835 863.108 29.3521 955.933 42.4774C1048.76 55.6026 1127.64 161.968 1181.5 209C1235.36 256.032 1141.5 339.257 1107.5 536.5C1073.5 733.743 1026.41 743.073 894.999 715C763.592 686.927 768.216 646.971 653.999 536.5C539.782 426.029 175.772 442.439 58.4989 379Z",
     }
-
-    path += ' Z'; // Close the path
-    return path;
-}
-
-
-  // Use Svelte's tweened store to animate the interpolation
-  const progress = tweened(0, {
-    duration: 2000,
-    easing: cubicInOut,
-  });
-
-  let potato: any;
-
-  onMount(() => {
-    potato = generateRandomPotatoShape();
-  });
-
-  $: interpolatedPath = $progress === 1 ? endPath : pathInterpolator($progress);
+  }
+  const potato2: PotatoShapeList = {
+    1: {
+      startPath: "M42.3636 121.721C118.651 32.0592 180.214 -28.6467 99.1727 60.1919C162.311 -8.12253 220.712 -1.73047 290.364 2.2214C360.864 6.2214 413.864 111.221 506.364 133.721C598.864 156.221 698.364 141.721 742.864 145.721C787.364 149.721 890.364 305.221 952.864 371.721C1015.36 438.221 912.364 511.221 833.864 553.721C755.364 596.221 490.364 492.721 395.364 371.721C300.364 250.721 149.364 400.221 76.3635 353.721C3.36355 307.221 -34.6364 212.221 42.3636 121.721Z",
+      endPath: "M105.003 12.4541C104.836 12.6339 104.669 12.8159 104.5 13C104.667 12.8157 104.834 12.6337 105.003 12.4541C114.932 1.75314 122.474 -1.33262 127.927 1.69087C216.179 -6.91215 432.627 221.884 496.364 225.5C566.864 229.5 558 452.5 650.5 475C743 497.5 826 610.5 870.5 614.5C915 618.5 1192 700 1254.5 766.5C1317 833 1179 860.222 1100.5 902.722C1022 945.222 764 799.5 669 678.5C574 557.5 208.5 824.5 135.5 778C62.5 731.5 -50.9995 553.5 26.0005 463C93.726 383.401 168.414 24.1404 127.927 1.69087C118.816 2.57899 111.072 5.9971 105.003 12.4541Z",
+    }
+  }
 </script>
 
 <div class="background">
-  <svg viewBox="0 0 1200 600" width="1000" height="500" xmlns="http://www.w3.org/2000/svg">
-    <path d="M89.5703 253.252C-27.7027 189.813 -4.78294 120.906 20.0469 58.9258C49.4606 19.1856 134.264 -7.06486 222.123 1.68529C309.982 10.4354 428.783 16.2689 583.11 17.7272C737.437 19.1856 774.109 13.3522 866.934 26.4774C959.759 39.6026 1030.43 73.874 1084.29 120.906C1138.15 167.938 1133.57 290.076 1099.57 487.318C1065.57 684.561 944.097 618.571 812.69 590.497C681.283 562.424 717.191 542.007 602.974 431.536C488.757 321.066 206.843 316.691 89.5703 253.252Z">
-      <animateTransform attributeName="transform" type="scale" begin="0s" dur="3s" values="1;1.1;1" repeatCount="indefinite"/>
-    </path>
-  </svg>
-  <svg viewBox="0 0 1200 600" width="900" height="700" xmlns="http://www.w3.org/2000/svg">
-    <path xmlns="http://www.w3.org/2000/svg" d="M42.3636 121.721C118.651 32.0592 180.214 -28.6467 99.1727 60.1919C162.311 -8.12253 220.712 -1.73047 290.364 2.2214C360.864 6.2214 413.864 111.221 506.364 133.721C598.864 156.221 698.364 141.721 742.864 145.721C787.364 149.721 890.364 305.221 952.864 371.721C1015.36 438.221 912.364 511.221 833.864 553.721C755.364 596.221 490.364 492.721 395.364 371.721C300.364 250.721 149.364 400.221 76.3635 353.721C3.36355 307.221 -34.6364 212.221 42.3636 121.721Z">
-      <animateTransform attributeName="transform" type="scale" begin="0s" dur="3s" values="1;1.1;1" repeatCount="indefinite"/>
-    </path>
-  </svg>
+  <PotatoShape potato={potato1} duration={8000} key={1} width={1200} height={800} />
+  <PotatoShape potato={potato2} duration={12000} key={1} width={1200} height={800} />
 </div>
 
 
@@ -71,26 +36,26 @@
     position: absolute;
     top: 0;
     left: 0;
-    svg {
-      fill: #6790F9;
-      position: absolute;
-      fill-opacity: 0.6;
-      animation: movePotato 10s infinite; /* Adjust time for speed */
-    }
-    svg:nth-child(1) {
-      right: 5rem;
-      top: 5rem;
-    }
-    svg:nth-child(2) {
-      left: 0rem;
-      bottom: 0rem;
-    }
-    @keyframes movePotato {
-      0% { transform: translate(0px, 0px); }
-      25% { transform: translate(10px, -20px); }
-      50% { transform: translate(20px, 0px); }
-      75% { transform: translate(10px, 20px); }
-      100% { transform: translate(0px, 0px); }
-    }
+  }
+  :global(svg) {
+    fill: #6790F9;
+    position: absolute;
+    fill-opacity: 0.4;
+    animation: movePotato 5s infinite; /* Adjust time for speed */
+  }
+  :global(svg:nth-child(1)) {
+    right: 5rem;
+    top: 5rem;
+  }
+  :global(svg:nth-child(2)) {
+    left: 0rem;
+    bottom: 0rem;
+  }
+  @keyframes movePotato {
+    0% { transform: translate(0px, 0px); }
+    25% { transform: translate(10px, -20px); }
+    50% { transform: translate(20px, 0px); }
+    75% { transform: translate(10px, 20px); }
+    100% { transform: translate(0px, 0px); }
   }
 </style>
---
+++ src/PotatoShape.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { interpolatePath } from 'd3-interpolate-path';
+  import { tweened } from 'svelte/motion';
+  import { cubicInOut } from 'svelte/easing';
+  import { onMount } from 'svelte';
+  interface PotatoShapeList {
+    [key: number]: {
+      startPath: string;
+      endPath: string;
+    }
+  }
+
+  export let potato: PotatoShapeList;
+  export let duration: number;
+  export let key: number;
+  export let width: number;
+  export let height: number;
+  
+  const pathInterpolator = interpolatePath(potato[key].startPath, potato[key].endPath);
+
+  const progress = tweened(0, {
+    duration: duration,
+    easing: cubicInOut,
+  });
+  // progress.subscribe((value) => console.log(value));
+
+  let interpolatedPath = potato[key].startPath;
+
+  // Animate the interpolation
+  $: $progress, interpolatedPath = pathInterpolator($progress);
+  // $: console.log($progress);
+
+  // Restart the animation once it completes
+  $: if ($progress >= 1) {
+    progress.set(0); // Reset progress to start the animation again
+  }
+  $: if ($progress <= 0) {
+    progress.set(1); // Reset progress to start the animation again
+  }
+</script>
+
+
+<svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+  <path d={interpolatedPath} />
+</svg>
