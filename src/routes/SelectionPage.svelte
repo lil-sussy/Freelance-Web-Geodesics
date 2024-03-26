436df2f5913119001d9210f82081e240f10c87e6
lil-sussy <naykeysnet@gmail.com>
2024-03-26 02:09:06 +0100

    selection page done almost

--- src/routes/SelectionPage.svelte
+++ src/routes/SelectionPage.svelte
@@ -3,22 +3,18 @@
   import logo from '$lib/images/webgeodesicslogo.svg';
   import arrow from '$lib/images/arrow.svg';
 </script>
-<p class="text-wrapper">I AM LOOKING FOR A</p>
-<div class="div-2">
-  <div class="WEB-DEVELOPER">WEB<br />DEVELOPER<br />&amp; DESIGNER</div>
-  <div class="overlap-2">
-    <div class="overlap-group-wrapper">
-      <div class="overlap-3">
-        <div class="rectangle-2"></div>
-        <div class="overlap-group-2">
-          <div class="rectangle-3"></div>
-          <div class="rectangle-4"></div>
-        </div>
-        <div class="rectangle-5"></div>
-      </div>
+
+<div class="webdevcontainer webdev">
+  <div class="WEB-DEVELOPER">WEB<br />DEVELOPER<br />& DESIGNER</div>
+  <img class="arrow" src={arrow} alt="arrow"/>
+  <div class="side-container">
+    <div class="overlap-3">
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="right"></div>
+      <div class="bottom"></div>
     </div>
-    <div class="div-3">
-      <p class="WEB-MOBILE-UX-UI-WEB">WEB &amp; MOBILE / UX.UI / WEB DEVELOPER</p>
+    <div class="content-container">
       <p class="transform-your-ideas">
         <span class="span">Transform Your Ideas into Digital Excellence: Let&#39;s </span>
         <span class="text-wrapper-2">Design</span>
@@ -26,146 +22,221 @@
         <span class="text-wrapper-2">Develop</span>
         <span class="span"> the Website Your Brand Deserves.&nbsp;&nbsp; </span>
       </p>
-    </div>
-  </div>
-  <img class="vector-6" src={arrow} alt="arrow"/>
-</div>
-<div class="div-4">
-  <div class="DATA-ANALYST">DATA<br />ANALYST</div>
-  <div class="overlap-2">
-    <div class="overlap-group-wrapper">
-      <div class="overlap-3">
-        <div class="rectangle-2"></div>
-        <div class="overlap-group-2">
-          <div class="rectangle-3"></div>
-          <div class="rectangle-4"></div>
-        </div>
-        <div class="rectangle-5"></div>
+      <div class="bottom-content-container">
+        <p class="WEB-MOBILE-UX-UI-WEB main top">WEB & MOBILE / UX.UI / WEB DEVELOPER</p>
+        <p class="WEB-MOBILE-UX-UI-WEB top">BASED IN PARIS</p>
+        <p class="WEB-MOBILE-UX-UI-WEB">CURRENTLY AVAILABLE FOR FREELANCE WORLDWIDE</p>
+        <p class="WEB-MOBILE-UX-UI-WEB">BORN IN PARIS</p>
       </div>
     </div>
-    <div class="div-5">
-      <p class="p">WEB &amp; MOBILE / UX.UI / WEB DEVELOPER / DATA SCIENTIST</p>
-      <p class="deciphering-data">
+  </div>
+</div>
+<h2 class="title">I AM LOOKING FOR A</h2>
+<div class="webdevcontainer data-analyst">
+  <div class="WEB-DEVELOPER">DATA<br />ANALYST</div>
+  <img class="arrow" src={arrow} alt="arrow"/>
+  <div class="side-container">
+    <div class="overlap-3">
+      <div class="top"></div>
+      <div class="bottom"></div>
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+    <div class="content-container">
+      <p class="transform-your-ideas">
         <span class="text-wrapper-3"
           >Deciphering Data, Crafting Narratives: Turn Numbers into Impactful Business
         </span>
         <span class="text-wrapper-2">Insights</span>
       </p>
+      <div class="bottom-content-container">
+        <p class="WEB-MOBILE-UX-UI-WEB main top">DATA SCIENTIST / DATA ANALYST / DATA ENGINEER</p>
+        <p class="WEB-MOBILE-UX-UI-WEB top">BASED IN PARIS</p>
+        <p class="WEB-MOBILE-UX-UI-WEB">CURRENTLY AVAILABLE FOR FREELANCE WORLDWIDE</p>
+        <p class="WEB-MOBILE-UX-UI-WEB">BORN IN PARIS</p>
+      </div>
     </div>
   </div>
-  <img class="vector-7" src={arrow} alt="arrow"/>
 </div>
 
 
 <style lang="scss">
-  .vector {
-    position: absolute;
-    width: 1244px;
-    height: 614px;
-    top: 0;
-    left: 676px;
-  }
 
-  .img {
-    position: absolute;
-    width: 716px;
-    height: 767px;
-    top: 320px;
-    left: 0;
-  }
-
-  .vector-2 {
-    position: absolute;
-    width: 1025px;
-    height: 679px;
-    top: 341px;
-    left: 828px;
-  }
-
-  .vector-3 {
-    position: absolute;
-    width: 1260px;
-    height: 563px;
-    top: 307px;
-    left: 186px;
-  }
-
-  .vector-4 {
-    position: absolute;
-    width: 447px;
-    height: 198px;
-    top: 251px;
-    left: 201px;
-  }
-
-  .vector-5 {
-    position: absolute;
-    width: 447px;
-    height: 198px;
-    top: 736px;
-    left: 201px;
-  }
-
-  .rectangle {
-    position: absolute;
-    width: 1920px;
-    height: 1080px;
-    top: 15px;
-    left: 1px;
-    background-color: #0000001a;
-    backdrop-filter: blur(200px) brightness(100%);
-    -webkit-backdrop-filter: blur(200px) brightness(100%);
-  }
-
-  .div {
-    position: absolute;
-    width: 963px;
-    height: 10px;
-    top: 620px;
-    left: 890px;
-    background-color: #ffffff;
-    border-radius: 14px;
-  }
-
-  .text-wrapper {
-    position: absolute;
-    top: 569px;
-    left: 55px;
-    font-family: "Segoe UI-Bold", Helvetica;
+  .title {
+    font-size: 5rem;
+    font-family: 'Segeo UI', sans-serif;
     font-weight: 700;
+    height: 10%;
     color: #ffffff;
-    font-size: 77px;
-    letter-spacing: 0;
-    line-height: normal;
+    transition: transform 0.5s;
+  }
+  .title::after {
+    margin-left: 3rem;
+    content: '';
+    position: absolute;
+    width: 50%;
+    height: 0.4rem;
+    border-radius: 10rem;
+    background-color: #ffffff;
+    margin-top: 1rem;
+  }
+  .webdevcontainer.data-analyst {
+    margin-top: 2%;
+  }
+  :global(#section2):has(.data-analyst:hover) {
+    .webdevcontainer {
+      transform: translateY(-15rem);
+    }
+    .title {
+      transform: translateY(-15rem);
+    }
+  }
+  // .data-analyst:hover ~ .webdev {
+  //   height: 10%;
+  //   transform: translateY(-50%);
+  // }
+  .webdevcontainer:hover {
+    opacity: 1;
+    cursor: pointer;
+    margin-left: 2%;
+    height: 55%;
+    .WEB-DEVELOPER {
+      color: #deec48;
+      margin-right: 10%;
+    }
+    .arrow {
+      margin-left: 5%;
+    }
+    .overlap-3 {
+      opacity: 0;
+    }
+    .side-container .content-container .bottom-content-container {
+      .WEB-MOBILE-UX-UI-WEB {
+        opacity: 0.7;
+        height: 50%;
+      }
+      .top {
+        opacity: 1;
+      }
+    }
+  }
+  .webdevcontainer {
+    .WEB-DEVELOPER {
+      transition: color 0.5s, margin-right 0.5s;
+    }
+    .arrow {
+      transition: margin-left 0.5s, margin-right 2s;
+    }
+    .overlap-3 {
+      transition: opacity 0.5s;
+    }
+    transition: height 0.5s, margin-left 0.5s, transform 0.5s;
+    width: 84%;
+    margin-left: 8%;
+    margin-right: 8%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    box-sizing: border-box;
+    height: 30%;
+    margin-top: 5%;
+    margin-bottom: 5%;
+    opacity: 0.7;
+    .WEB-DEVELOPER {
+      font-family: "Stretch Pro", Helvetica;
+      margin-top: 3rem;
+      font-weight: 400;
+      color: #ffffff;
+      width: 30%;
+      font-size: 40px;
+      letter-spacing: 0;
+      line-height: normal;
+    }
+    .side-container {
+      position: relative;
+      width: 70%;
+      height: 100%;
+      .content-container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        padding: 3rem;
+        box-sizing: border-box;
+        width: 100%;
+        height: 100%;
+        .bottom-content-container {
+          transition: margin-top 0.5s;
+          margin-top: 2rem;
+          display: flex;
+          gap: 2rem 1rem;
+          flex-wrap: wrap;
+          width: 50%;
+          .WEB-MOBILE-UX-UI-WEB {
+            width: 11rem;
+            font-family: "Segoe UI", Helvetica;
+            font-weight: 400;
+            color: #ffffff;
+            font-size: 16px;
+            letter-spacing: 0;
+            line-height: normal;
+            opacity: 0;
+            height: 0;
+            transition: opacity 0.5s, height 0.5s;
+          }
+          .main {
+            height: 50%;
+            opacity: 1;
+          }
+        }
+        .transform-your-ideas {
+          font-family: "Acme-Regular", Helvetica;
+          font-weight: 400;
+          color: transparent;
+          font-size: 40px;
+          letter-spacing: 0;
+          line-height: 40px;
+        } 
+      }
+      .overlap-3 {
+        position: relative;
+        height: 283px;
+        .left {
+          position: absolute;
+          width: 2px;
+          height: 100%;
+          top: 0%;
+          left: 1rem;
+          background-color: #ffffff;
+        }
+        .bottom {
+          position: absolute;
+          width: 100%;
+          height: 2px;
+          bottom: 1rem;
+          left: 0%;
+          background-color: #ffffff;
+        }
+        .top {
+          position: absolute;
+          width: 100%;
+          height: 2px;
+          top: 1rem;
+          left: 0rem;
+          background-color: #ffffff;
+        }
+        .right {
+          position: absolute;
+          width: 2px;
+          height: 100%;
+          top: 0%;
+          right: 1rem;
+          background-color: #ffffff;
+        }
+      }
+    }
   }
 
-  .div-2 {
-    position: absolute;
-    width: 1503px;
-    height: 283px;
-    top: 237px;
-    left: 307px;
-  }
 
-  .WEB-DEVELOPER {
-    position: absolute;
-    top: 78px;
-    left: 0;
-    font-family: "Stretch Pro-Regular", Helvetica;
-    font-weight: 400;
-    color: #ffffffb2;
-    font-size: 40px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .overlap-2 {
-    position: absolute;
-    width: 1121px;
-    height: 283px;
-    top: 0;
-    left: 380px;
-  }
 
   .overlap-group-wrapper {
     position: absolute;
@@ -173,20 +244,6 @@
     height: 283px;
     top: 0;
     left: 0;
-  }
-
-  .overlap-3 {
-    position: relative;
-    height: 283px;
-  }
-
-  .rectangle-2 {
-    position: absolute;
-    width: 2px;
-    height: 283px;
-    top: 0;
-    left: 31px;
-    background-color: #d9d9d9;
   }
 
   .overlap-group-2 {
@@ -197,85 +254,20 @@
     left: 0;
   }
 
-  .rectangle-3 {
-    position: absolute;
-    width: 2px;
-    height: 1121px;
-    top: -349px;
-    left: 560px;
-    background-color: #d9d9d9;
-    transform: rotate(-90deg);
-  }
-
-  .rectangle-4 {
-    position: absolute;
-    width: 2px;
-    height: 1121px;
-    top: -560px;
-    left: 560px;
-    background-color: #d9d9d9;
-    transform: rotate(-90deg);
-  }
-
-  .rectangle-5 {
-    position: absolute;
-    width: 2px;
-    height: 283px;
-    top: 0;
-    left: 1084px;
-    background-color: #d9d9d9;
-  }
-
-  .div-3 {
-    position: absolute;
-    width: 1019px;
-    height: 166px;
-    top: 58px;
-    left: 51px;
-  }
-
-  .WEB-MOBILE-UX-UI-WEB {
-    position: absolute;
-    width: 169px;
-    top: 124px;
-    left: 0;
-    font-family: "Segoe UI-Regular", Helvetica;
-    font-weight: 400;
-    color: #ffffff;
-    font-size: 16px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .transform-your-ideas {
-    position: absolute;
-    width: 1015px;
-    top: 0;
-    left: 0;
-    font-family: "Acme-Regular", Helvetica;
-    font-weight: 400;
-    color: transparent;
-    font-size: 40px;
-    letter-spacing: 0;
-    line-height: 40px;
-  }
-
   .span {
-    color: #ffffffc2;
+    color: #ffffff;
+    font-family: 'Acme' sans-serif;
   }
 
   .text-wrapper-2 {
-    font-family: "Segoe Script-Bold", Helvetica;
+    font-family: "Segoe Script", Helvetica;
     font-weight: 700;
     color: #deec48;
   }
 
   .vector-6 {
-    position: absolute;
     width: 168px;
     height: 40px;
-    top: 220px;
-    left: 0;
   }
 
   .div-4 {
@@ -285,27 +277,6 @@
     top: 728px;
     left: 307px;
   }
-
-  .DATA-ANALYST {
-    position: absolute;
-    top: 113px;
-    left: 0;
-    font-family: "Stretch Pro-Regular", Helvetica;
-    font-weight: 400;
-    color: #ffffffb2;
-    font-size: 40px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .div-5 {
-    position: absolute;
-    width: 989px;
-    height: 166px;
-    top: 65px;
-    left: 51px;
-  }
-
   .p {
     position: absolute;
     width: 228px;
@@ -318,94 +289,12 @@
     letter-spacing: 0;
     line-height: normal;
   }
-
-  .deciphering-data {
-    position: absolute;
-    width: 985px;
-    top: 0;
-    left: 0;
-    font-family: "Acme-Regular", Helvetica;
-    font-weight: 400;
-    color: transparent;
-    font-size: 40px;
-    letter-spacing: 0;
-    line-height: 40px;
-  }
-
   .text-wrapper-3 {
     color: #ffffffbf;
   }
 
-  .vector-7 {
-    position: absolute;
+  .arrow {
     width: 168px;
     height: 40px;
-    top: 229px;
-    left: 0;
-  }
-
-  .navbar {
-    position: absolute;
-    width: 1571px;
-    height: 37px;
-    top: 97px;
-    left: 312px;
-  }
-
-  .text-wrapper-4 {
-    position: absolute;
-    top: 0;
-    left: 0;
-    font-family: "Segoe UI-Bold", Helvetica;
-    font-weight: 700;
-    color: #ffffff;
-    font-size: 24px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .text-wrapper-5 {
-    left: 480px;
-    position: absolute;
-    top: 0;
-    font-family: "Segoe UI-Regular", Helvetica;
-    font-weight: 400;
-    color: #ffffff;
-    font-size: 24px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .text-wrapper-6 {
-    left: 982px;
-    position: absolute;
-    top: 0;
-    font-family: "Segoe UI-Regular", Helvetica;
-    font-weight: 400;
-    color: #ffffff;
-    font-size: 24px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .text-wrapper-7 {
-    left: 1459px;
-    position: absolute;
-    top: 0;
-    font-family: "Segoe UI-Regular", Helvetica;
-    font-weight: 400;
-    color: #ffffff;
-    font-size: 24px;
-    letter-spacing: 0;
-    line-height: normal;
-  }
-
-  .rectangle-6 {
-    position: absolute;
-    width: 148px;
-    height: 1px;
-    top: 36px;
-    left: 0;
-    background-color: #ffffff;
   }
 </style>
