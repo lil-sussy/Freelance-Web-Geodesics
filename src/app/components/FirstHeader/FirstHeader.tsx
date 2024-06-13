504b3593a6f14472fd993e44e70de51d848e0f9b
= <=>
2024-06-13 23:30:30 +0200

    first header redesign

--- src/app/components/FirstHeader/FirstHeader.tsx
+++ src/app/components/FirstHeader/FirstHeader.tsx
@@ -3,40 +3,45 @@ import Button from "../Button/Button";
 import { Arrow } from "../Icons";
 import styles from "./FirstHeader.module.scss";
 
-const FirstHeader = () => {
+const FirstHeader: React.FC = () => {
 	return (
-		<div className={styles.Header}>
-			<div className={styles.Content}>
-				<div className={styles.Space}></div>
-				<div className={styles.ContentContainer}>
-					<div className={styles.MediumLengthHeroHeadlineGoesHere}>Expert Prompt Engineering and AI Solutions</div>
-					<div className={styles.LoremIpsumDolorSitAmetConsecteturAdipiscingElitSuspendisseVariusEnimInErosElementumTristiqueDuisCursusMiQuisViverraOrnareErosDolorInterdumNullaUtCommodoDiamLiberoVitaeErat}>
+		<header className={styles.header}>
+			<div className={styles.content}>
+				<div className={styles.space}></div>
+				<div className={styles.contentContainer}>
+					<h1 className={styles.headline}>Expert Prompt Engineering and AI Solutions</h1>
+					<p className={styles.description}>
 						<span>Transforming your tech ideas into reality with precision and </span>
-						<span className={styles.creative}>innovation</span>.
-            <br></br>
-						<span>Custom AI integrations tailored to streamline your business operations.
-              <br></br> Full-stack development </span>
-						<span className={styles.innovative}>expertise</span>
+						<span className={styles.highlight}>innovation</span>.
+						<br />
+						<span>
+							Custom AI integrations tailored to streamline your business operations.
+							<br />
+							Full-stack development{" "}
+						</span>
+						<span className={styles.highlight}>expertise</span>
 						<span> from Python to React.</span>
-					</div>
-					<div className={styles.Actions}>
+					</p>
+					<div className={styles.actions}>
 						<Button style="primary">Contact</Button>
 						<Button style="secondary">See my work</Button>
 					</div>
 				</div>
 			</div>
-			<div className={styles.Logos}>
-				<img src={"./images/webgeodesicslogo.svg"} className={styles.artboard} alt="web geodesics logo" />
-				<div className={styles.WebGeodesics}>
-					WEB
-					<br />
-					GEODESICS
-				</div>
+			<div className={styles.logos}>
+        <div className={styles.wrapper}>
+          <img src={"./images/webgeodesicslogo.svg"} className={styles.logoImage} alt="web geodesics logo" />
+          <div className={styles.logoText}>
+            WEB
+            <br />
+            GEODESICS
+          </div>
+        </div>
 				<div className={styles.arrow}>
 					<Arrow />
 				</div>
 			</div>
-		</div>
+		</header>
 	);
 };
 
